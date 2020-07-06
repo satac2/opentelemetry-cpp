5d3116f89093322877864907f71430330ede113b
Sam Atac <samatac@google.com>
2020-07-06 14:38:53 +0000

    style and formatting

--- sdk/include/opentelemetry/sdk/context/context.h
+++ sdk/include/opentelemetry/sdk/context/context.h
@@ -1,97 +1,99 @@
 #pragma once
 
-#include "opentelemetry/context/context.h"
 #include "opentelemetry/common/attribute_value.h"
+#include "opentelemetry/context/context.h"
 
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <map>
 #include <atomic>
+#include <map>
+#include <string>
 
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace sdk
 {
-  namespace context
+namespace context
+{
+class Context final : public opentelemetry::context::Context
+{
+public:
+  // The Key class is used to obscure access from the
+  // user to the context map. The identifier is used as a key
+  // to the context map.
+  class Key : opentelemetry::context::Context::Key
   {
-    class Context final : public opentelemetry::context::Context
-    {
-      public:
 
-        // The Key class is used to obscure access from the
-        // user to the context map. The identifier is used as a key
-        // to the context map.
-        class Key :  opentelemetry::context::Context::Key 
-      {
+  public:
+    // Returns the key identifier.
+    std::string GetIdentifier();
 
-        public:
-          std::string GetIdentifier();
-          
-          std::string GetName();
+    // Returns the key name.
+    std::string GetName();
 
-          Key();
+    Key();
 
-          Key(std::string key_name);
+    Key(std::string name);
 
-        private:
-          friend class Context;
+  private:
+    friend class Context;
 
-          std::string key_name_;
-          
-          std::string identifier_;
-          //char identifier_[50];
-      };
+    std::string name_;
 
-        // The Id class is used to keep track of the next avaiable
-        // context id number.
-        class Id {
-          private:
-          friend class Context;
+    std::string identifier_;
+  };
 
-            static std::atomic<int> next_;
-            Id();
+  // The Id class is used to keep track of the next avaiable
+  // context id number.
+  class Id
+  {
+  private:
+    friend class Context;
 
-            int GetNext(); 
-        };
-        
+    // Contains the next available context id number.
+    static std::atomic<int> next_;
 
-        /* Creates a key with the passed in name and returns it. */
-        Key CreateKey(nostd::string_view key_name);
+    Id();
 
-        Context();
+    // Returns the next available context id number.
+    int GetNext();
+  };
 
-        template <class T, nostd::enable_if_t<trace::detail::is_key_value_iterable<T>::value> * = nullptr>  
-          Context(const T &attributes){
-            id_ = Id().GetNext();
-            key_vals_.insert(attributes.begin(), attributes.end());
-          }
+  // Creates a key with the passed in name and returns it.
+  Key CreateKey(std::string key_name);
 
+  Context();
 
-        template <class T, nostd::enable_if_t<trace::detail::is_key_value_iterable<T>::value> * = nullptr>
-          Context WriteValues(T &attributes)
-          { 
-            attributes.insert(key_vals_.begin(), key_vals_.end());
-
-            return Context(attributes);
-          }
-
-        /* Returns the value associated with the passed in key */
-        common::AttributeValue GetValue(Key key);
-        
-        Context(const Context &other){
-          key_vals_ = other.key_vals_;
-          id_ = other.id_;
-        }
-
-        bool operator==(const Context &other);
-
-      private:
-        int id_;
-        std::map<std::string, common::AttributeValue> key_vals_;
-
-    };
-        std::atomic<int> Context::Id::next_(1);
+  // Constructs a new context object with the passed in keys and values.
+  template <class T, nostd::enable_if_t<trace::detail::is_key_value_iterable<T>::value> * = nullptr>
+  Context(const T &attributes)
+  {
+    id_ = Id().GetNext();
+    key_vals_.insert(attributes.begin(), attributes.end());
   }
-}
-OPENTELEMETRY_END_NAMESPACE
 
+  // Accepts an iterable, combines it with the existing iterable
+  // and constructs a new context object contains the two combined.
+  template <class T, nostd::enable_if_t<trace::detail::is_key_value_iterable<T>::value> * = nullptr>
+  Context WriteValues(T &attributes)
+  {
+    T attr_copy = attributes;
+    attr_copy.insert(key_vals_.begin(), key_vals_.end());
+
+    return Context(attr_copy);
+  }
+
+  // Returns the value associated with the passed in key.
+  common::AttributeValue GetValue(Key key);
+
+  Context(const Context &other);
+
+  bool operator==(const Context &other);
+
+private:
+  int id_;
+
+  std::map<std::string, common::AttributeValue> key_vals_;
+};
+
+std::atomic<int> Context::Id::next_(1);
+}  // namespace context
+}  // namespace sdk
+OPENTELEMETRY_END_NAMESPACE
